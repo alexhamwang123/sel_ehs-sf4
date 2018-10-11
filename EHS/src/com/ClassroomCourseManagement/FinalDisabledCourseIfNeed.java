20adb8ed0eafd0dddb9e3d0ba0f6587d5ff4ade5
Kevin <khuang@trismax.com>
2018-10-11 23:51:29 +0800

    support multiple acitve to be inactived at all

--- EHS/src/com/ClassroomCourseManagement/FinalDisabledCourseIfNeed.java
+++ EHS/src/com/ClassroomCourseManagement/FinalDisabledCourseIfNeed.java
@@ -1,16 +1,19 @@
 package com.ClassroomCourseManagement;
-
 import org.apache.commons.text.RandomStringGenerator;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.awt.*;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+
+import java.security.Key;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -58,120 +61,117 @@ public class FinalDisabledCourseIfNeed {
         driver.findElement(By.name("submit")).click();
 
         Thread.sleep(4500);
+        Boolean isBreak = false;
+        restartLoop:
+        while (true) {
+            JavascriptExecutor js = (JavascriptExecutor) driver;
 
-        JavascriptExecutor js = (JavascriptExecutor)driver;
+            WebElement courseAdmin = driver.findElement(By.xpath("//*[@id=\"navPrimary\"]/li[7]/ul/li[3]/a"));
+            js.executeScript("arguments[0].click()", courseAdmin);
 
-        WebElement courseAdmin = driver.findElement(By.xpath("//*[@id=\"navPrimary\"]/li[7]/ul/li[3]/a"));
-        js.executeScript("arguments[0].click()", courseAdmin);
+            Thread.sleep(2000);
+            driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[1]/div/a[3]")).click();
+            Thread.sleep(1500);
 
-        Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@id=\"content\"]/div/div[1]/div/a[3]")).click();
-        Thread.sleep(1500);
-
-        Thread.sleep(1500);// find element
-        WebElement inputOne = driver.findElement(By.className("form-control"));
+            Thread.sleep(1500);// find element
+            WebElement inputOne = driver.findElement(By.className("form-control"));
 
 // to set focus?
-        inputOne.click();
-        // erase any existing value (because clear does not send any events
-        for (int i = 0; i < inputOne.getAttribute("value").length(); i++) {
-            inputOne.sendKeys(Keys.BACK_SPACE);
-        }
+            inputOne.click();
+            // erase any existing value (because clear does not send any events
+            for (int i = 0; i < inputOne.getAttribute("value").length(); i++) {
+                inputOne.sendKeys(Keys.BACK_SPACE);
+            }
 
 // type in value
-        inputOne.sendKeys("Course Category");
+            inputOne.sendKeys("Course Category");
 
 
 // click away to fire blur event
-        driver.findElement(By.className("form-control")).click();
-        Thread.sleep(1500);
-        WebElement inputSec = driver.findElement(By.className("form-control"));
+            driver.findElement(By.className("form-control")).click();
+            Thread.sleep(1500);
+            WebElement inputSec = driver.findElement(By.className("form-control"));
 // to set focus?
-        inputSec.click();
-        for (int i = 0; i < inputSec.getAttribute("value").length(); i++) {
-            inputSec.sendKeys(Keys.BACK_SPACE);
-        }
-        inputSec.sendKeys(Keys.TAB);
-//        Survey_Only_New
-        new Select(driver.findElement(By.id("secondmenu")).findElement(By.id("searchCategory"))).selectByVisibleText("Survey_Only_Sub_Category");//By.xpath("//option[@value='f9bbb962d5b1aa58b2115a7bf3b4c9a8']")));
-        Thread.sleep(1500);
-        driver.findElement(By.xpath("//*[@id=\"EHSForm\"]/div/div/input")).click();
-        Thread.sleep(1500);
-
-        java.util.List<WebElement> myResults;
-
-        myResults = driver.findElements(By.tagName("td"));
-        Boolean isBreak=false;
-        int $i = 0;
-if (myResults.size() > 0){
-    System.out.println("There is one record above.");
-
-   // this.myresultList($i,myResults,driver);
-//    for (WebElement myResult : myResults) {
-    for(int $p=0; $p<myResults.size();$p++){
-        if(isBreak) break;
-    WebElement myResult4 = (WebElement) myResults.get($p);
-    String idstr2 = myResult4.getText();
-    if("Active".equals(idstr2)) {
-        int $k=0;
-        $k = $p;
-        $p = $p - 3;
-        System.out.println(idstr2);
-        WebElement myResult5 = (WebElement) myResults.get($p);
-        String idstr5 = myResult5.getText();
-
-        java.util.List<WebElement> myResults3a = driver.findElements(By.tagName("a"));
-        int $j = 0;
-        $p = $k;
-        if (myResults3a.size() > 0) {
-            for (WebElement myResult3a : myResults3a) {
-                String idstr3a = myResult3a.getText();
-                String id3a = myResult3a.getAttribute("id");
-
-                if (idstr5.equals(idstr3a)) {
-                    // System.out.println("$ j is " + $j);
-
-                    driver.findElement(By.xpath("//a[@id='" + id3a + "']")).click();
-                    Thread.sleep(1500);
-                    driver.findElement(By.name("detailCourseIsActive")).click();
-                    Thread.sleep(1500);
-                    driver.findElement(By.name("langIsViewable")).click();
-                    Thread.sleep(1900);
-                    driver.findElement(By.cssSelector("input[type='submit'][value='Yes']")).click();
-                    Thread.sleep(2500);
-                    driver.findElement(By.cssSelector("input[type='button'][value='Save']")).click();
-                    Thread.sleep(1500);
-                    driver.findElement(By.partialLinkText("Courses")).click();
-                    Thread.sleep(1500);
-                    driver.findElement(By.id("srch_fld")).sendKeys(idstr3a);
-                    Thread.sleep(1500);
-                    driver.findElement(By.name("searchButton")).click();
-                    Thread.sleep(1500);
-                    String currentWin = driver.getWindowHandle();
-                    try {
-                        driver.findElement(By.className("onelang")).click();
-                    } catch (NoSuchElementException e) {
-                        isBreak = true;
-                        break ;
-                        //Assert.assertEquals(true, true);
-//                           Assert.fail("something went wrong while creating and uploading the scorm course");
-                    }
-                    Thread.sleep(1500);
-
-                }
-
+            inputSec.click();
+            for (int i = 0; i < inputSec.getAttribute("value").length(); i++) {
+                inputSec.sendKeys(Keys.BACK_SPACE);
             }
-            $j++;
-    }
-    $p++;
-    }
-    }
+            inputSec.sendKeys(Keys.TAB);
+//        Survey_Only_New
+            new Select(driver.findElement(By.id("secondmenu")).findElement(By.id("searchCategory"))).selectByVisibleText("Survey_Only_Selenium");//By.xpath("//option[@value='f9bbb962d5b1aa58b2115a7bf3b4c9a8']")));
+            Thread.sleep(1500);
+            driver.findElement(By.xpath("//*[@id=\"EHSForm\"]/div/div/input")).click();
+            Thread.sleep(1500);
 
-}
-else {
-    System.out.println("There is no record at all.");
-}
+            java.util.List<WebElement> myResults;
 
+            myResults = driver.findElements(By.tagName("td"));
+            if (myResults.size() > 0) {
+                System.out.println("There is one record above.");
+                for (int $p = 0; $p <myResults.size(); $p++) {
+                    if (isBreak) break;
+                    WebElement myResult4;
+                    myResult4 = (WebElement) myResults.get($p);
+                    String idstr2 = myResult4.getText();
+                    if ("Active".equals(idstr2)) {
+                        //we have to  make one to be inactived at least if it mets.
+                        int $k = 0;
+                        $k = $p;
+                        $p = $p - 4;
+                        WebElement myResult5;
+                        myResult5 = (WebElement) myResults.get($p);
+                        String idstr5 = myResult5.getText();
+                        java.util.List<WebElement> myResults3a;
+                        myResults3a = driver.findElements(By.tagName("a"));
+                        int $j = 0;
+                        $p = $k;
+                        if (myResults3a.size() > 0) {
+                            for (WebElement myResult3a : myResults3a) {
+                                String idstr3a = myResult3a.getText();
+                                String id3a = myResult3a.getAttribute("id");
+                                if (idstr5.equals(idstr3a)) {
+                                    // System.out.println("$ j is " + $j);
+
+                                    driver.findElement(By.xpath("//a[@id='" + id3a + "']")).click();
+                                    Thread.sleep(1500);
+                                    driver.findElement(By.name("detailCourseIsActive")).click();
+                                    Thread.sleep(1500);
+                                    driver.findElement(By.name("langIsViewable")).click();
+                                    Thread.sleep(1900);
+                                    driver.findElement(By.cssSelector("input[type='submit'][value='Yes']")).click();
+                                    Thread.sleep(1500);
+                                    driver.findElement(By.cssSelector("input[type='button'][value='Save']")).click();
+                                    Thread.sleep(1000);
+                                    driver.findElement(By.partialLinkText("Courses")).click();
+                                    Thread.sleep(1500);
+                                    driver.findElement(By.id("srch_fld")).sendKeys(idstr3a);
+                                    Thread.sleep(1500);
+                                    driver.findElement(By.name("searchButton")).click();
+                                    Thread.sleep(1500);
+                                    String currentWin = driver.getWindowHandle();
+                                    try {
+                                        driver.findElement(By.className("onelang")).click();
+                                    } catch (NoSuchElementException e) {
+                                        continue restartLoop;
+                                    }
+                                    Thread.sleep(1500);
+
+                                }
+
+                            }
+                            $j++;
+                        }
+                        $p++;
+                    }
+                }// Nothing is active. So we have to break it out first.
+                isBreak = true;
+                //we should break it again.
+                if (isBreak)break;
+            } else {
+                System.out.println("There is no record at all.");
+                break;
+            }
+        }
         robot.keyPress(KeyEvent.VK_META);
         robot.keyPress(KeyEvent.VK_SHIFT);
         robot.keyPress(KeyEvent.VK_G);
@@ -193,9 +193,21 @@ else {
 
         robot.keyPress(KeyEvent.VK_ENTER);
         robot.keyRelease(KeyEvent.VK_ENTER);
+
         Thread.sleep(1500);
+        driver.findElement(By.id("lightbox")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("input[type='submit'][value='OK']")).click();
+        Thread.sleep(2000);
+        driver.findElement(By.id("login_login_id")).sendKeys(username);
+        driver.findElement(By.id("login_password")).sendKeys(password);
+
+        driver.findElement(By.name("submit")).click();
+        Thread.sleep(500);
+
         driver.quit();
-        }
+
+    }
 
 }
 
