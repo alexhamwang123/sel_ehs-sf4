117007978282538ccf046b01704e599ce6923350
trismax <dwang@trismax-dev.com>
2018-11-14 12:21:19 +0800

    CreateChecklist

--- EHS/src/com/ChecklistManagement/CreateChecklist.java
+++ EHS/src/com/ChecklistManagement/CreateChecklist.java
@@ -40,7 +40,7 @@ public class CreateChecklist {
 		prop.load(inStream);
 		String urladdr = prop.getProperty("url");
 		driver.get(urladdr);
-		driver.manage().window().maximize();
+		//driver.manage().window().maximize();
 		RandomStringGenerator generator = new RandomStringGenerator.Builder().withinRange('0', 'z').filteredBy(LETTERS, DIGITS).build();;
 		String username = prop.getProperty("username");
 		String password = prop.getProperty("password");
@@ -52,7 +52,7 @@ public class CreateChecklist {
 		
 		Thread.sleep(4500);
 		
-		WebElement courseAdmin = driver.findElement(By.xpath("//*[@id=\"navPrimary\"]/li[7]/ul/li[3]/a"));
+		WebElement courseAdmin = driver.findElement(By.xpath("//a[contains(text(),'Course Admin')]"));
 		JavascriptExecutor js = (JavascriptExecutor)driver;
 
 		js.executeScript("arguments[0].click();", courseAdmin);
@@ -67,9 +67,9 @@ public class CreateChecklist {
 		new Select(driver.findElement(By.id("detailCategoryType"))).selectByVisibleText("Survey_Only_New");
 		new Select(driver.findElement(By.id("detailCourseType"))).selectByVisibleText("Checklist");
 		new Select(driver.findElement(By.id("detailCourseExpiration"))).selectByVisibleText("Never Expires");
-		Thread.sleep(1000);
+		Thread.sleep(3000);
 		driver.findElement(By.id("saveBtn")).click();
-		Thread.sleep(3500);
+		Thread.sleep(10000);
 		driver.findElement(By.cssSelector("input[type='button'][value='Edit']")).click();
 		Thread.sleep(1500);
 		driver.findElement(By.id("detailCheckListTitle")).sendKeys("test checklist title");
@@ -79,11 +79,11 @@ public class CreateChecklist {
 		driver.findElement(By.id("detailInstructionalText")).sendKeys("gratz dude");
 		Thread.sleep(1000);
 		driver.findElement(By.cssSelector("input[type='button'][value='Save']")).click();
-		Thread.sleep(2000);
+		Thread.sleep(5000);
 		driver.findElement(By.id("createContent")).click();
 		Thread.sleep(1000);
 		driver.findElement(By.cssSelector("input[type='submit'][value='Create']")).click();
-		Thread.sleep(1500);
+		Thread.sleep(5000);
 		driver.findElement(By.cssSelector("input[type='button'][value='Edit']")).click();
 		Thread.sleep(1500);
 		driver.findElement(By.id("saveBtn")).click();
@@ -91,13 +91,15 @@ public class CreateChecklist {
 		driver.findElement(By.id("fancyConfirm_ok")).click();
 		Thread.sleep(1500);
 		driver.findElement(By.cssSelector("input[type='button'][value='Back']")).click();
-		Thread.sleep(1500);
+		Thread.sleep(4000);
 		driver.findElement(By.cssSelector("input[type='button'][value='Save']")).click();
-		Thread.sleep(1500);
+		Thread.sleep(3500);
 		driver.findElement(By.cssSelector("input[type='button'][value='Back']")).click();
 		Thread.sleep(3500);
+		driver.findElement(By.cssSelector("input[type='button'][value='Back']")).click();
+		Thread.sleep(7000);
 		driver.findElement(By.id("langIsViewable")).click();
-		Thread.sleep(1500);
+		Thread.sleep(5000);
 		driver.findElement(By.id("detailIsActive")).click();
 		Thread.sleep(1500);
 		driver.findElement(By.cssSelector("input[type='button'][value='Save']")).click();
@@ -108,7 +110,8 @@ public class CreateChecklist {
 		driver.findElement(By.name("searchButton")).click();
 		Thread.sleep(1500);
 		driver.findElement(By.className("onelang")).click();
-		Thread.sleep(1500);
+		Thread.sleep(10000);
+
 		driver.findElement(By.id("crselink1")).click();
 		Thread.sleep(3500);
 		driver.quit();
